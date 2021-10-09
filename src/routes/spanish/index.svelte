40ea79b3422972c213426900a702fb96512f7541
Lee Norris <lee.norris@arcadia.io>
2021-10-09 08:44:22 -0400

    chore: all voices

--- src/routes/spanish/index.svelte
+++ src/routes/spanish/index.svelte
@@ -4,16 +4,21 @@
 <button on:click={click}>say word</button>
 
 <script lang="ts">
-    function speak(text, pitch, lang = 'en') {
+    function speak(text, pitch, rate, voice, lang = 'en') {
         let speech = new SpeechSynthesisUtterance();
         speech.lang = lang;
         speech.pitch = pitch;
         speech.text = text;
+        speech.rate = rate;
+        speech.voice = voice;
         window.speechSynthesis.speak(speech);
     }
 
     function click() {
-        speak('i like', -100000)
-        speak('queso apestoso', 1000, 'es-419')
+        window.speechSynthesis.getVoices().map(v => {
+            speak('no daipers', 100000, 11111, v)
+            // speak('underpants', -10000, 1, v)
+            speak('underpants', -1000000, 1, v)
+        })
     }
 </script>
