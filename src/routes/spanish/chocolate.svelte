1b64e0c8e6de2b7fb913fbd5315a5057652851ec
Lee Norris <lee.norris@arcadia.io>
2021-10-09 08:23:48 -0400

    chore: worx

---
+++ src/routes/spanish/chocolate.svelte
@@ -0,0 +1,2 @@
+chocolate
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7CTAXSLbmtYgSmDTgWZ41gQpwmvuY9Fs5ng&usqp=CAU" />
---
+++ src/routes/spanish/index.svelte
@@ -0,0 +1,19 @@
+<a href="spanish/chocolate/">chocolate</a>
+<a href="spanish/leche/">leche</a>
+
+<button on:click={click}>say word</button>
+
+<script lang="ts">
+    function speak(text, pitch, lang = 'en') {
+        let speech = new SpeechSynthesisUtterance();
+        speech.lang = lang;
+        speech.pitch = pitch;
+        speech.text = text;
+        window.speechSynthesis.speak(speech);
+    }
+
+    function click() {
+        speak('i like', -100000)
+        speak('queso apestoso', 1000, 'es-419')
+    }
+</script>
